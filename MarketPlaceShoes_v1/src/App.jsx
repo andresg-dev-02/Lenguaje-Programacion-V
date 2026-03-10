e0bb13de289865861804417af4f063a9f8e8a730
Juan Sebastián Montaño Pérez <sp6027193@gmail.com>
2026-03-09 23:04:33 -0500

    feat: Add project dependencies and a React SVG asset.

---
+++ MarketPlaceShoes_v1/src/App.jsx
@@ -0,0 +1,15 @@
+import { useState } from 'react'
+import Nav from './components/Nav'
+
+function App() {
+
+  return (
+    <>
+      <div>
+        <Nav />
+      </div >
+    </>
+  )
+}
+
+export default App
---
+++ MarketPlaceShoes_v1/src/components/Nav.jsx
@@ -0,0 +1,15 @@
+function Nav() {
+    return (
+        <div>
+            <nav>
+                <ul>
+                    <li>Home</li>
+                    <li>Products</li>
+                    <li>Services</li>
+                    <li>About</li>
+                    <li>Contact</li>
+                </ul>
+            </nav>
+        </div>
+    )
+}
