551346c4611b71daa4d5ae23b3a4bcae238ab6a3
AlejandroSotoL <alejandro12.soto.lopez@gmail.com>
2026-03-12 10:50:28 -0500

    feat: Configure application to listen on a port specified by the PORT environment variable, defaulting to 10000.

--- Program.cs
+++ Program.cs
@@ -73,6 +73,7 @@ builder.Services.AddSwaggerGen(s =>
 
 var app = builder.Build();
 
+
 // Configure the HTTP request pipeline.
 if (app.Environment.IsDevelopment())
 {
--- obj/Debug/net10.0/MarketPlace.AssemblyInfo.cs
+++ obj/Debug/net10.0/MarketPlace.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("MarketPlace")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+11a994d0f6afa0023baa0cec2b285de9d0288942")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+bb2ac8d8bb4c74d8f4a61358d2cccbe5dd59f9cb")]
 [assembly: System.Reflection.AssemblyProductAttribute("MarketPlace")]
 [assembly: System.Reflection.AssemblyTitleAttribute("MarketPlace")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
